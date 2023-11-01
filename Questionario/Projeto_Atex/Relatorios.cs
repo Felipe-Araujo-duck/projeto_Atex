85467cc7577d338220bde6919d07ef1cc5893a7c
sondrigo <rodrigo.himegame@hotmail.com>
2023-10-31 22:11:59 -0300

    João Pedro

--- Questionario/Projeto_Atex/Relatorios.cs
+++ Questionario/Projeto_Atex/Relatorios.cs
@@ -7,14 +7,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using Projeto_Atex.Data;
 
 namespace Projeto_Atex
 {
-    public partial class Relatorios : Form
+    public partial class Relatorios : MetroFramework.Forms.MetroForm
     {
         public Relatorios()
         {
             InitializeComponent();
+        }
+
+        public DataTools _tools;
+
+        private void tbnPesquisar_Click(object sender, EventArgs e)
+        {
+            _tools = new DataTools();
+            SqlConnection conn = _tools.ConnectionDB();
+            gridCrianca.DataSource = _tools.DbSet("Select * from Crianca",conn);
         }
     }
 }
