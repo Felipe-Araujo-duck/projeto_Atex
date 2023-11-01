33a6c566d2c71271d54e9f05f628a2260e55bb2e
JoaoMarceloB <joaomarcelobatistanarciso@gmail.com>
2023-10-31 21:36:33 -0300

    adicionado a classe DataTools e as entidades do banco

---
+++ Questionario/Projeto_Atex/Data/Entity/Crianca.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Atex.Data.Entity
+{
+    public class Crianca
+    {
+        public int IdCrianca { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public int IdResponsavel { get; set; }
+        public int IdEscola { get; set; }
+    }
+}
---
+++ Questionario/Projeto_Atex/Data/Entity/JogoRedeSocial.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Atex.Data.Entity
+{
+    public class JogoRedeSocial
+    {
+        public int IdJogoRedeSocial { get; set; }
+        public string Nome { get; set; }
+        public string Dominio { get; set; }
+        public string Tipo { get; set; }
+    }
+}
